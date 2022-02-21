95ce30f6e375530c1ba8bfae4b6e8c128a561edb
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-21 22:43:08 +0100

    Add can_be_archived? method

--- classes/item.rb
+++ classes/item.rb
@@ -1,9 +1,17 @@
+require 'date'
+
 class Item
   attr_reader :publish_date
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1..10_000)
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = archived
+  end
+
+  def can_be_archived?
+    return true if DateTime.now - @publish_date > 10
+
+    false
   end
 end
--- spec/classes/item_spec.rb
+++ spec/classes/item_spec.rb
@@ -1,10 +1,21 @@
-require_relative '../../classes/item.rb'
+require_relative '../../classes/item'
+require 'date'
 
 describe Item do
   context 'When created' do
-    item = Item.new("2020-12-04")
+    item = Item.new('2020-12-04')
     it 'should have a date' do
-      expect(item.publish_date).to eql "2020-12-04"
+      expect(item.publish_date).to be_instance_of(Date)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    context 'when called then' do
+      item = Item.new('2007-12-04')
+
+      it 'should return true if the publish_date is older than 10' do
+        expect(item.can_be_archived?).to be true
+      end
     end
   end
 end
