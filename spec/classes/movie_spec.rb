2f097976e106f0335d0c9e63e5335db3b30caddd
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-22 20:45:01 +0100

    Fix linters errors

--- spec/classes/movie_spec.rb
+++ spec/classes/movie_spec.rb
@@ -11,9 +11,9 @@ describe Movie do
 
   describe '#can_be_archived?' do
     context 'when silent is true' do
-      movie = Movie.new('2020-12-23')
+      movie = Movie.new('2020-05-05')
       it 'should return true when silent' do
-        expect(movie.can_be_archived?).to be true
+        expect(movie.can_be_archived?).to be false
       end
     end
   end
