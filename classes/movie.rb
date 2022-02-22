933eaf1e4a27c733b9d2b19f6be0d66b1624e6b2
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-22 18:53:47 +0100

    Add tests for Movie

--- classes/movie.rb
+++ classes/movie.rb
@@ -2,6 +2,7 @@ require_relative 'item'
 
 class Movie < Item
   attr_reader :silent
+  attr_accessor :archive, :publish_date
 
   def initialize(publish_date, silent: false, archived: false)
     super(publish_date, archived: archived)
@@ -9,6 +10,6 @@ class Movie < Item
   end
 
   def can_be_archived?
-    @silent || can_be_archived?
+    return true if super || @silent
   end
 end
