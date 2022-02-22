d4bcf4c94a2936d374a7bcac92ad088436a7a09d
eapenzacharias <eapenzac@gmail.com>
2022-02-22 19:20:45 +0100

    `can_be_archived?` method added

--- classes/game.rb
+++ classes/game.rb
@@ -1,10 +1,14 @@
 require_relative './item.rb'
 
 class Game < Item
-  attr_reader :publish_date, :author
+  attr_reader :multiplayer, :last_played_at
   def initialize(publish_date:, multiplayer:, last_played_at:, archived: false )
     super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    super && ((Date.today - @last_played_at).to_i / 365.00) > 2.00
   end
 end
