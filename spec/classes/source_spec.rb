d68c34ad5a23e5b1d382f0721bf92ae788bf37b7
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-22 21:20:52 +0100

    Update source tests

--- spec/classes/source_spec.rb
+++ spec/classes/source_spec.rb
@@ -12,4 +12,17 @@ describe Source do
       expect(source.items).to be_empty
     end
   end
+
+  
+  describe '#add_item' do
+    context 'when added' do
+      source = Source.new('Emma')
+      it 'should add an item to items' do
+        item_mock = double('movie')
+        allow(item_mock).to receive_message_chain(:source=)
+        source.add_item(item_mock)
+        expect(source.items).to include item_mock
+      end
+    end
+  end
 end
