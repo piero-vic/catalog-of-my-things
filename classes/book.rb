9dad21ac69cadbb5f9a60b334d936915f01336cc
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-22 11:36:37 -0500

    Access publisher and cover_state variables

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,8 @@
 require_relative './item'
 
 class Book < Item
+  attr_reader :publisher, :cover_state
+
   def initialize(publisher, cover_state, publish_date, archived: false)
     super(publish_date, archived: archived)
     @publisher = publisher
