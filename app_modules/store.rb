4f8a5263ec8027edc7de631e644e48e86d88905b
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-02-23 18:27:33 -0300

    add store_music_albums method

--- app_modules/store.rb
+++ app_modules/store.rb
@@ -18,4 +18,13 @@ module Store
     end
     File.write('./data/movies.json', JSON.dump(movies_data))
   end
+
+  def store_music_albums()
+    music_album_data = @items.grep(MusicAlbum).map do |item|
+      { on_spotify: item.on_spotify, publish_date: item.publish_date,
+        archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
+        source: item.source.id }
+    end
+    File.write('./data/music_albums.json', JSON.dump(music_album_data))
+  end
 end
