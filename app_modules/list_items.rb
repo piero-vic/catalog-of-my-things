31e1b251634dcad707ed73e46056805929511c3b
eapenzacharias <eapenzac@gmail.com>
2022-02-23 17:02:51 +0100

    added `print_item_props` for displaying common item properties

--- app_modules/list_items.rb
+++ app_modules/list_items.rb
@@ -1,6 +1,11 @@
 module ListItems
+  def print_item_props(item)
+    puts "Publish date: #{item.publish_date}"
+  end
+
   def list_books
     @items.grep(Book).each do |book|
+      print_item_props(book)
       puts "Publisher: #{book.publisher}"
       print "Cover State: #{book.cover_state}\n\n"
     end
@@ -8,6 +13,7 @@ module ListItems
 
   def list_games
     @items.grep(Game).each do |game|
+      print_item_props(game)
       puts "Multiplayer: #{game.multiplayer ? 'yes' : 'no'}"
       print "Last time played: #{game.last_played_at}\n\n"
     end
@@ -15,22 +21,24 @@ module ListItems
 
   def list_movies
     @items.grep(Movie).each do |movie|
+      print_item_props(movie)
       print "Silent: #{movie.silent ? 'yes' : 'no'}\n\n"
     end
   end
 
   def list_music_albums
     @items.grep(MusicAlbum).each do |album|
+      print_item_props(album)
       print "Is it on Spotify?: #{album.on_spotify ? 'yes' : 'no'}\n\n"
     end
   end
 
   def list_items
     menu = <<~HEREDOC
-      [1] List a book
-      [2] List a music album
-      [3] List a movie
-      [4] List a game
+      [1] List all books
+      [2] List all music albums
+      [3] List all movies
+      [4] List all games
       [5] Return to main menu
     HEREDOC
     puts menu
@@ -39,11 +47,11 @@ module ListItems
     when 1
       list_books
     when 2
-      list_games
+      list_music_albums
     when 3
       list_movies
     when 4
-      list_music_albums
+      list_games
     end
   end
 end
