da00662bc2670285b98ebe054c7b516fd2ce5e48
eapenzacharias <eapenzac@gmail.com>
2022-02-23 18:55:21 +0100

    `add_item_props` method added

--- app_modules/add_props.rb
+++ app_modules/add_props.rb
@@ -1,9 +1,16 @@
 module AddProps
+  def add_item_props(item)
+    add_genre(item)
+    add_label(item)
+    add_author(item)
+    add_source(item)
+  end
+
   def add_genre(item)
     print "\nEnter genre: "
     genre_name = gets.chomp.downcase
-    item.genre = @item_props.select do |prop|
-                   prop.name == genre_name && prop.instance_of?(Genre)
+    item.genre = @item_props.grep(Genre).select do |prop|
+                   prop.name == genre_name
                  end [0] || Genre.new(genre_name)
     @item_props.push(item.genre) unless @item_props.include?(item.genre)
   end
@@ -13,8 +20,8 @@ module AddProps
     label_title = gets.chomp.downcase
     print "\nEnter color: "
     label_color = gets.chomp.downcase
-    item.label = @item_props.select do |prop|
-                   prop.title == label_title && prop.color == label_color && prop.instance_of?(Label)
+    item.label = @item_props.grep(Label).select do |prop|
+                   prop.title == label_title && prop.color == label_color
                  end [0] || Label.new(label_title, label_color)
     @item_props.push(item.label) unless @item_props.include?(item.label)
   end
@@ -22,8 +29,8 @@ module AddProps
   def add_author(item)
     print "\nEnter author's fullname: "
     first_name, last_name = gets.chomp.downcase.split
-    item.author = @item_props.select do |prop|
-                    prop.first_name == first_name && prop.last_name == last_name && prop.instance_of?(Author)
+    item.author = @item_props.grep(Author).select do |prop|
+                    prop.first_name == first_name && prop.last_name == last_name
                   end [0] || Author.new(first_name, last_name)
     @item_props.push(item.author) unless @item_props.include?(item.author)
   end
@@ -31,9 +38,9 @@ module AddProps
   def add_source(item)
     print "\nEnter source: "
     source_name = gets.chomp.downcase
-    item.source = @item_props.select do |prop|
-                   prop.name == source_name && prop.instance_of?(Source)
-                 end [0] || Source.new(source_name)
+    item.source = @item_props.grep(Source).select do |prop|
+                    prop.name == source_name
+                  end [0] || Source.new(source_name)
     @item_props.push(item.source) unless @item_props.include?(item.source)
   end
 end
