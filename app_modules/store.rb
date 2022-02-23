7a205d5be6eab37666a426fabe7b40239d26fd04
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-23 22:20:19 +0100

    Add store_movies method

--- app_modules/store.rb
+++ app_modules/store.rb
@@ -9,4 +9,13 @@ module Store
     end
     File.write('./data/books.json', JSON.dump(books_data))
   end
+
+  def store_movies()
+    movies_data = @items.grep(Movie).map do |item|
+      { silent: item.silent, publish_date: item.publish_date,
+        archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
+        source: item.source.id }
+    end
+    File.write('./data/movies.json', JSON.dump(movies_data))
+  end
 end
