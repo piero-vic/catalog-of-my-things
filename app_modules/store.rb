f550dbec15af7d6c0adf1e5ae16e91ec223b3d73
eapenzacharias <eapenzac@gmail.com>
2022-02-23 22:35:41 +0100

    added `store_games` method

--- app_modules/store.rb
+++ app_modules/store.rb
@@ -1,30 +1,39 @@
 require 'json'
 
 module Store
-  def store_books()
-    books_data = @items.grep(Book).map do |item|
+  def store_books
+    data = @items.grep(Book).map do |item|
       { publisher: item.publisher, cover_state: item.cover_state, publish_date: item.publish_date,
         archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
         source: item.source.id }
     end
-    File.write('./data/books.json', JSON.dump(books_data))
+    File.write('./data/books.json', JSON.dump(data))
   end
 
-  def store_movies()
-    movies_data = @items.grep(Movie).map do |item|
+  def store_movies
+    data = @items.grep(Movie).map do |item|
       { silent: item.silent, publish_date: item.publish_date,
         archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
         source: item.source.id }
     end
-    File.write('./data/movies.json', JSON.dump(movies_data))
+    File.write('./data/movies.json', JSON.dump(data))
   end
 
-  def store_music_albums()
-    music_album_data = @items.grep(MusicAlbum).map do |item|
+  def store_music_albums
+    data = @items.grep(MusicAlbum).map do |item|
       { on_spotify: item.on_spotify, publish_date: item.publish_date,
         archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
         source: item.source.id }
     end
-    File.write('./data/music_albums.json', JSON.dump(music_album_data))
+    File.write('./data/music_albums.json', JSON.dump(data))
+  end
+
+  def store_games
+    data = @items.grep(Game).map do |item|
+      { multiplayer: item.multiplayer, last_played_at: item.last_played_at, publish_date: item.publish_date,
+        archived: item.archived, genre: item.genre.id, author: item.author.id, label: item.label.id,
+        source: item.source.id }
+    end
+    File.write('./data/games.json', JSON.dump(data))
   end
 end
