fdc45346093a0c75436f0607fd7c2af9a0e4d91e
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-23 20:28:57 +0100

    Add list_props and list_genre

---
+++ app_modules/list_props.rb
@@ -0,0 +1,29 @@
+module ListProps
+  def list_genres
+    @item_props.grep(Genre).each_with_index do |prop, index|
+      print "#{index + 1}) #{prop.name.capitalize}\n\n"
+    end
+  end
+
+  def list_props
+    menu = <<~HEREDOC
+      [1] List all genres
+      [2] List all labels
+      [3] List all authors
+      [4] List all sources
+      [5] Return to main menu
+    HEREDOC
+    puts menu
+    user_input = gets.chomp.to_i
+    case user_input
+    when 1
+      list_genres
+    when 2
+      list_music_albums
+    when 3
+      list_movies
+    when 4
+      list_games
+    end
+  end
+end
