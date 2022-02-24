7a9319ea3dec7d5181db0c0c2b155cc59e748768
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-24 12:10:52 -0500

    Add books table

--- schema.sql
+++ schema.sql
@@ -20,15 +20,15 @@ CREATE TABLE label (
   color VARCHAR(50) NOT NULL
 ); 
 
-CREATE TABLE musicalbum (
- id SERIAL INT PRIMARY KEY,
- on_spotify BOOLEAN NOT NULL,
- publish_date DATE NOT NULL,
- archived BOOLEAN NOT NULL,
- genre_id INT REFERENCES genre(id),
- author_id INT REFERENCES author(id),
- label_id INT REFERENCES label(id),
- source_id INT REFERENCES source(id)
+CREATE TABLE music_album (
+  id SERIAL INT PRIMARY KEY,
+  on_spotify BOOLEAN NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  source_id INT REFERENCES source(id)
 );
 
 CREATE TABLE games (
@@ -46,6 +46,18 @@ CREATE TABLE games (
 CREATE TABLE movies (
   id SERIAL INT PRIMARY KEY,
   silent BOOLEAN NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  source_id INT REFERENCES source(id)
+);
+
+CREATE TABLE books (
+  id SERIAL INT PRIMARY KEY,
+  publisher VARCHAR(50) NOT NULL,
+  cover_state VARCHAR(50) NOT NULL,
   publish_date DATE NOT NULL,
   archived BOOLEAN NOT NULL,
   genre_id INT REFERENCES genre(id),
