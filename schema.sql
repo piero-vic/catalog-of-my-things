1d2dd8d6a38b19e18d8b95faaf11388e8290db2b
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-24 18:03:09 +0100

    Create movies table

--- schema.sql
+++ schema.sql
@@ -42,3 +42,14 @@ CREATE TABLE games (
   label_id INT REFERENCES label(id),
   source_id INT REFERENCES source(id)
 );
+
+CREATE TABLE movies (
+  id SERIAL INT PRIMARY KEY,
+  silent BOOLEAN NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  source_id INT REFERENCES source(id)
+);
