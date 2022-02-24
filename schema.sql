134cb73621011fee12b0996845c950c42b29704f
eapenzacharias <eapenzac@gmail.com>
2022-02-24 17:49:45 +0100

    added `games` table

--- schema.sql
+++ schema.sql
@@ -12,10 +12,22 @@ CREATE TABLE source (
 CREATE TABLE genre (
   id SERIAL INT PRIMARY KEY,
   name VARCHAR(50) NOT NULL
-); 
+);
 
 CREATE TABLE label (
   id SERIAL INT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
   color VARCHAR(50) NOT NULL
-); 
+);
+
+CREATE TABLE games (
+  id SERIAL INT PRIMARY KEY,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  source_id INT REFERENCES source(id)
+);
